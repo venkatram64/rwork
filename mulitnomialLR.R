353d15146d8ea0fb83f0733f2e626fa32806f55f
venkatram64 <venkat.veerareddy@hotmail.com>
2019-06-03 08:43:46 +0530

    init commit

--- mulitnomialLR.R
+++ mulitnomialLR.R
@@ -4,6 +4,27 @@ mydata <- read.csv("Cardiotocographic.csv", header = T)
 print(mydata)
 print(str(mydata))
 
+#multinomial logistic regression model
 mydata$NSPF <- factor(mydata$NSP)
 mydata$out <- relevel(mydata$NSPF, ref="1")
 library(nnet)
+
+mymodel <- multinom(out ~ LB+AC+FM, data = mydata)
+print(summary(mymodel))
+
+#predict
+#p1 <- predict(mymodel, mydata)
+p1 <- predict(mymodel, mydata, type="prob")
+print(p1)
+
+#misclassification error
+cm <- table(predict(mymodel), mydata$NSPF)
+print(cm)
+
+error <- 1-sum(diag(cm))/sum(cm)
+print(error)
+
+#2-tailed z test
+z <- summary(mymodel)$coefficients/summary(mymodel)$standard.errors
+p <- (1-pnorm(abs(z), 0, 1)) * 2
+print(p)
